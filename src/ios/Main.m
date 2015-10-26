50cce1181cdd15e2ff75f9426fd455bd7ae2636e
Pranav Gupta <pranavguptarulz@gmail.com>
2015-10-27 00:49:11 +0530

    Getting publickey from init options

--- src/ios/Main.m
+++ src/ios/Main.m
@@ -8,7 +8,7 @@
 
     NSDictionary * options = [NSJSONSerialization JSONObjectWithData:[[[command arguments] objectAtIndex:0] dataUsingEncoding:NSUTF8StringEncoding] options:0 error:nil];
     
-    RazorpayPayment * co = [[RazorpayPayment alloc] initWithKey:@"rzp_test_1DP5mmOlF5G5ag"];
+    RazorpayPayment * co = [[RazorpayPayment alloc] initWithKey: (NSString *)[options objectForKey:@"key"]];
     co.main = self;
     co.callbackId = [command callbackId];
     [co open:options];
