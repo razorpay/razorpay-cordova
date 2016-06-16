1573715d48ad1a101e76f2286088e00264ba742a
akshaybhalotia <akshay.bhalotia@razorpay.com>
2016-06-16 13:17:09 +0530

    Android integration changes

--- src/android/Main.java
+++ src/android/Main.java
@@ -1,6 +1,6 @@
 package com.razorpay.cordova;
 
-import com.razorpay.cordova.Fragment;
+import com.razorpay.Checkout;
 import org.json.JSONObject;
 import org.apache.cordova.*;
 import org.json.JSONArray;
@@ -8,10 +8,13 @@ import org.json.JSONException;
 import android.widget.Toast;
 
 public class Main extends CordovaPlugin {
+
+  CallbackContext c;
+
   @Override
   public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-    Fragment co = new Fragment();
-    co.setCallbackContext(callbackContext);
+    Checkout co = new Checkout();
+    c = callbackContext;
     try{
       JSONObject options = new JSONObject(data.getString(0));
       co.setPublicKey(options.getString("key"));
@@ -20,5 +23,19 @@ public class Main extends CordovaPlugin {
       Toast.makeText(this.cordova.getActivity(), e.getMessage(), Toast.LENGTH_LONG).show();
     }
     return true;
+  }
+
+  public void onPaymentSuccess(String razorpay_payment_id) {
+    c.success(razorpay_payment_id);
+  }
+
+  public void onPaymentError(int code, String response) {
+    JSONObject error = new JSONObject();
+    try{
+      error.put("code", code);
+      error.put("description", response);
+    } catch(Exception e){
+    }
+    c.error(error);
   }
 }
