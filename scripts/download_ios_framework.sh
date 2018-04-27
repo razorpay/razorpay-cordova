32e9804542462686b9d87c540c947446fb85366a
Shashank <34152717+iThink32@users.noreply.github.com>
2018-04-27 14:26:26 +0530

    Revert "iOS Release v1.3.7"

---
+++ scripts/download_ios_framework.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+## Run this script after installing the module
+
+curl -O http://downloads.razorpay.com/Razorpay.framework-0.15.1.zip
+unzip -o ./Razorpay.framework-0.15.1.zip
+rm -r ./src/ios/Razorpay.framework
+cp -R ./Razorpay.framework ./src/ios/
